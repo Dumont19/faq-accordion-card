6a1689f40d66433ff2eb4d328c9c68d74a8802e6
Matheus Dumont <95390248+Dumont19@users.noreply.github.com>
2022-08-16 02:25:02 -0300

    desktop design finished

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,10 @@
-const btns = document.querySelectorAll('button')
-
-function showAnswers() {
-    btns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            btn.classList.toggle('active')
-
-            let answer = btn.nextElementSibling
-            let arrow = btn.children[0]
-
-            if (answer.style.display === 'block') {
-                answer.style.display = 'none'
-                arrow.style.transform = 'rotate(0deg)'
-            } else {
-                answer.style.display = 'block'
-                arrow.style.transform = 'rotate(180deg)'
-            }
-        })
-    })
-}
-showAnswers()
+document.querySelectorAll('.question').forEach(question =>
+	question.addEventListener('click', () => {
+		if (question.parentNode.classList.contains('active')) {
+			question.parentNode.classList.toggle('active')
+		} else {
+			document.querySelectorAll('.question').forEach(question => question.parentNode.classList.remove('active'))
+			question.parentNode.classList.add('active')
+		}
+	})
+)
